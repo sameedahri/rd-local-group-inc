5a23803e55eaddd1e5acc987d15c514d02e0b33c
AbdulQadir92 <a.qadir.jamali@gmail.com>
2024-10-25 18:56:02 +0500

    created login page for desktop

---
+++ src/components/login/LoginCard.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import logo from "/public/assets/images/login/logo.png";
+
+
+const LoginCard = () => {
+    const router = useRouter();
+
+    return(
+        <div className="w-1/3 bg-white rounded-[12px] p-9 mt-16">
+            <div className="flex justify-center mt-4 mb-10">
+                <Image src={logo} alt="Logo" width={100} height={95} />
+            </div>
+            <p className="text-black text-center font-gilroySemibold text-[32px] leading-9">Welcome! Just enter your Business phone number</p>
+            <p className="mt-3 text-[#3F3F3F0] font-gilroyMedium text-center">Glad to see you, Login to your account below</p>
+            <div className="mt-12 flex flex-col gap-2">
+                <label htmlFor="phoneNumber" className="text-[#3F3F3F] font-gilroyMedium text-sm">Enter your Phone number*</label>
+                <input type="tel" placeholder="+1 1234 5678 90" className="w-[100%] h-[52px] border border-[#DFDFDF] rounded-[5px] p-3" />
+            </div>
+            <button 
+                className="w-[100%] h-[52px] bg-loginBg rounded-[5px] text-loginBtn font-gilroySemibold text-center text-base leading-8 mt-3"
+                type="button"
+                onClick={() => router.push("/")}
+            >Login</button>
+        </div>
+    )
+
+}
+
+export default LoginCard
